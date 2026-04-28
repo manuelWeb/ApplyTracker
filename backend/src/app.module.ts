bf08f5548abd10cbf3198cbe38f04995e93bd190
Manu2015 <devries.webdesigner@gmail.com>
2026-04-28 15:29:54 +0200

    feat(db): TypeORM client configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_CONTAINER_PORT),
+
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+
+  autoLoadEntities: true,
+
+  synchronize: false,
+};
