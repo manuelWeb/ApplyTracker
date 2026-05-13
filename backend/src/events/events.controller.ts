c890318fc9fd73b6db0167d8038466af04215c55
Manu2015 <devries.webdesigner@gmail.com>
2026-05-13 09:25:38 +0200

    feat(controller): add events read-only controller and test

---
+++ backend/src/events/events.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { EventsService } from './events.service';
+
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Get()
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.eventsService.findOne(id);
+  }
+}
