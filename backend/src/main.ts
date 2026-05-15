967c7ecc8db7f4aa3ec8ee5547aae3b806c8e327
Manu2015 <devries.webdesigner@gmail.com>
2026-05-15 15:50:53 +0200

    feat(api): setup swagger documentation
    - install @nestjs/swagger
    - configure Swagger in main.ts
    - expose documentation on /api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ApplyTracker API')
+    .setDescription('ApplyTracker backend API documentation')
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000, () => {
     console.log(
       `Server is running on port ${process.env.PORT ?? 3000} Docker api exposed port ${process.env.API_HOST_PORT ?? 3008}`,
