9c550cc308c39e9b226d75aa84dad93ec746a3d7
Manu2015 <devries.webdesigner@gmail.com>
2026-05-30 17:54:48 +0200

    test(applications): update read ownership unit tests

--- backend/src/applications/applications.controller.spec.ts
+++ backend/src/applications/applications.controller.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('ApplicationsController', () => {
   let controller: ApplicationsController;
 
   const service = {
-    findAll: jest.fn(),
-    findOne: jest.fn(),
+    findAllFromUser: jest.fn(),
+    findOneFromUser: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -26,27 +27,50 @@ describe('ApplicationsController', () => {
     jest.clearAllMocks();
   });
 
-  describe('findAll', () => {
+  describe('findAllFromUser', () => {
     it('should return applications from service', async () => {
+      const userId = 42;
       const result = [{ id: 1 }, { id: 2 }];
 
-      service.findAll.mockResolvedValue(result);
+      service.findAllFromUser.mockResolvedValue(result);
 
-      await expect(controller.findAll()).resolves.toEqual(result);
+      await expect(controller.findAllFromUser(userId)).resolves.toEqual(result);
 
-      expect(service.findAll).toHaveBeenCalledTimes(1);
+      expect(service.findAllFromUser).toHaveBeenCalledWith(42);
+      expect(service.findAllFromUser).toHaveBeenCalledTimes(1);
     });
   });
 
-  describe('findOne', () => {
+  describe('findOneFromUser', () => {
     it('should return application from service', async () => {
+      const userId = 42;
       const result = { id: 1 };
 
-      service.findOne.mockResolvedValue(result);
+      service.findOneFromUser.mockResolvedValue(result);
 
-      await expect(controller.findOne(1)).resolves.toEqual(result);
-      expect(service.findOne).toHaveBeenCalledTimes(1);
-      expect(service.findOne).toHaveBeenCalledWith(1);
+      await expect(controller.findOneFromUser(userId, 1)).resolves.toEqual(
+        result,
+      );
+      expect(service.findOneFromUser).toHaveBeenCalledTimes(1);
+      expect(service.findOneFromUser).toHaveBeenCalledWith(userId, 1);
+    });
+
+    it('should return 404 on application or user id not found', async () => {
+      const userId = 24;
+      const applicationId = 876;
+      const error = new NotFoundException(
+        `No application #${applicationId} for userId #${userId}`,
+      );
+      service.findOneFromUser.mockRejectedValue(error);
+
+      await expect(
+        controller.findOneFromUser(userId, applicationId),
+      ).rejects.toThrow(NotFoundException);
+      expect(service.findOneFromUser).toHaveBeenCalledTimes(1);
+      expect(service.findOneFromUser).toHaveBeenCalledWith(
+        userId,
+        applicationId,
+      );
     });
   });
 });
